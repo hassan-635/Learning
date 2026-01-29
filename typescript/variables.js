78aa253985991cc60e11fc1dfa9ff02edf66c639
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-01-29 17:22:11 +0500

    variables learnt

--- typescript/variables.js
+++ typescript/variables.js
@@ -1,3 +1,35 @@
-var a;
-var b;
-var c = a + b;
+//primitive datatypes ( number, string boolean)
+var a = 10;
+var b = "hello";
+var c = true;
+console.log(a, b, c);
+//non-primitive datatypes (array, object)
+var arr = [1, 2, 3, 4, 5];
+var obj = { name: "John", age: 20 };
+console.log(arr, obj);
+//union datatypes (number | string)
+var d = 10;
+var e = "hello";
+//tuples
+var f = [10, "hello"];
+console.log(f);
+//any
+var g = "hello";
+g = 10;
+console.log(g);
+//unknown
+var h = "hello";
+h = 10;
+console.log(h);
+//void
+var i = undefined;
+console.log(i);
+//null
+var k = null;
+console.log(k);
+//undefined
+var l = undefined;
+console.log(l);
+//symbol
+var m = Symbol("hello");
+console.log(m);
--- typescript/variables.ts
+++ typescript/variables.ts
@@ -1,3 +1,44 @@
-let a;
-let b;
-let c = a+b;
+//primitive datatypes ( number, string boolean)
+let a:number = 10;
+let b:string = "hello";
+let c:boolean = true;
+console.log(a,b,c);
+
+//non-primitive datatypes (array, object)
+let arr:number[] = [1,2,3,4,5];
+let obj:object = {name:"John", age:20};
+console.log(arr,obj);
+
+//union datatypes (number | string)
+let d:number | string = 10;
+let e:number | string = "hello";
+
+//tuples
+let f:[number, string] = [10, "hello"];
+console.log(f);
+
+//any
+let g:any = "hello";
+g = 10;
+console.log(g);
+
+//unknown
+let h:unknown = "hello";
+h = 10;
+console.log(h);
+
+//void
+let i:void = undefined;
+console.log(i);
+
+//null
+let k:null = null;
+console.log(k);
+
+//undefined
+let l:undefined = undefined;
+console.log(l);
+
+//symbol
+let m:symbol = Symbol("hello");
+console.log(m);
