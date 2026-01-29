311c5d8149ab99e26004359ab30a170b25189a7c
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-01-29 19:52:19 +0500

    interfaces

--- typescript/interfaces.js
+++ typescript/interfaces.js
@@ -4,3 +4,10 @@ function printUserData(obj) {
     console.log(obj.name, obj.email, obj.password);
 }
 printUserData({ name: "Hassan", email: "hassan@gmail.com", password: "123456" });
+var admin = { name: "Hassan", email: "hassan@gmail.com", password: "123456", role: "admin" };
+printUserData(admin);
+var userAndAdmin = { name: "Hassan", email: "hassan@gmail.com", password: "123456", role: "admin" };
+printUserData(userAndAdmin);
+var userOrAdmin = { name: "Hassan", email: "hassan@gmail.com", password: "123456", role: "admin" };
+printUserData(userOrAdmin);
+var userType = { name: "Hassan", email: "hassan@gmail.com", password: "123456" };
--- typescript/interfaces.ts
+++ typescript/interfaces.ts
@@ -13,3 +13,39 @@ function printUserData(obj: user): void {
 }
 
 printUserData({name: "Hassan", email: "hassan@gmail.com", password: "123456"});
+
+//extending interfaces
+
+interface admin extends user {
+    role: string
+}
+
+const admin: admin = {name: "Hassan", email: "hassan@gmail.com", password: "123456", role: "admin"};
+
+printUserData(admin);
+
+//intersection types
+
+type userAndAdmin = user & admin;
+
+const userAndAdmin: userAndAdmin = {name: "Hassan", email: "hassan@gmail.com", password: "123456", role: "admin"};
+
+printUserData(userAndAdmin);
+
+//union types
+
+type userOrAdmin = user | admin;
+
+const userOrAdmin: userOrAdmin = {name: "Hassan", email: "hassan@gmail.com", password: "123456", role: "admin"};
+
+printUserData(userOrAdmin);
+
+//type aliases
+
+type userType = {
+    name: string,
+    email: string,
+    password: string
+}
+
+const userType: userType = {name: "Hassan", email: "hassan@gmail.com", password: "123456"};
